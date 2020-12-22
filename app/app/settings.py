887fb3cb1084bbc2cffbdacf37161f4a4a349809
yeiipi <juanpab.sierra@urosario.edu.co>
2020-12-22 15:53:37 -0500

    Udate admin to support customm user model
    issue no: UD007

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+
+    # local
+    'core'
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
