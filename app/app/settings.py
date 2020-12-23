8aedbe5704149e5e9fdf42adda2501a581047602
yeiipi <juanpab.sierra@urosario.edu.co>
2020-12-23 01:22:42 -0500

    Fix engine mistake
    issue no: DB011
    
    backend ---> backends

--- app/app/settings.py
+++ app/app/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backend.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
